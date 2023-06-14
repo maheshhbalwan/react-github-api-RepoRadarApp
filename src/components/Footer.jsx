d66cc9a20ea9619aa829076a41db5621a5e402ed
maheshhbalwan <85355816+maheshhbalwan@users.noreply.github.com>
2023-06-15 04:52:53 +0530

    add: navbar & footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+
+
+export const Footer = () => {
+  return (
+    <footer className="bottom-0 left-0 right-0 py-2 text-center bg-gray-200">
+      <p>
+        Made with 💻 by{" "}
+        <a href="https://github.com/maheshhbalwan" className="hover:underline">Maheshh Balwan</a>
+      </p>
+    </footer>
+  );
+};
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export const Navbar = () => {
+  return (
+    <nav className="px-4 py-2 text-center bg-gray-800">
+      <div className="mx-auto max-w-7xl">
+        <h1 className="text-2xl font-bold text-white">Github Repo Radar</h1>
+      </div>
+    </nav>
+  );
+};
