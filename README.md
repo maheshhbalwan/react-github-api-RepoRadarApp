4a58448f2b3f42dd6db35181298a714549e647d9
maheshhbalwan <85355816+maheshhbalwan@users.noreply.github.com>
2023-06-14 17:33:32 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# react-github-api-RepoRadarApp
+ 
---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold text-center p-5">Github Repo Radar</h1>
+  );
+}
